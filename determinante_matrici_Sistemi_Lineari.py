a9158fcd397f4b0130014c0089a5d42bbb5a49d0
leoardo.r <leonardo.ramazzotti95@gmail.com>
2021-04-24 00:10:31 +0200

    English language integration

--- determinante_matrici_Sistemi_Lineari.py
+++ determinante_matrici_Sistemi_Lineari.py
@@ -7,21 +7,21 @@ Created on Tue Apr  6 16:18:36 2021
 """
 # Task: Devo fare ub programma che mi calcoli il determinante di una matrice complessa
 
-controllo=input('Devi fare operazioni? (si/no) \n ')
+controllo=input('You have to do something? (y/n) \n ')
 
-while controllo =='si':
+while controllo =='y':
 
-    n_R=int(input('Quante righe ha la matrice? '))
-    n_C=int(input('Quante colonne ha la matrice? '))
+    n_R=int(input('How many rows does the matrix have? '))
+    n_C=int(input('How many columns does the matrix have? '))
 
     n_it= n_R*n_C # moltiplico per trovare il numero di elementi della matrice
 
-    lista=[] 
+    lista=[]
 
     index=0
 
     while index< n_it:
-        z=complex(input('Inserisci un numero (anche complesso): (R+Ij)\n\n'))
+        z=complex(input('Enter a value (even copmlex): (R+Ij)\n\n'))
         lista.append(z)  # Aggiungo il numero a una lista per poi manipolarlo
         index +=1
         continue
@@ -31,42 +31,42 @@ while controllo =='si':
     import numpy as np
 
     x=np.array(lista) # prendo la lista di numeri e la trasformo in un vettore
-    m=np.reshape(x,[n_R,n_C]) # trasformo il vettore in una matrice 
-                
-    print('Matrice da manipolare: \n\n ',m,'\n\n')
+    m=np.reshape(x,[n_R,n_C]) # trasformo il vettore in una matrice
+
+    print('Matrix to manipulate: \n\n ',m,'\n\n')
 
 
 
     det=np.linalg.det(m) # libreria numpy determinante
 
-    print('Determinante: \n\n',det)
+    print('Determinant: \n\n',det)
 
-    quest=input('Devi risolvere una equazione lineare? ')
+    quest=input('You have to solve a linear system? (y/n) ')
 
-    if quest=='si':
-    
-        ter_noti=[] 
+    if quest=='y':
+
+        ter_noti=[]
 
         index=0
 
         while index< n_R:
-            t=complex(input('Inserisci un numero (anche complesso): (R+Ij)\n\n'))
+            t=complex(input('Enter a value (even complessalex): (R+Ij)\n\n'))
             ter_noti.append(t)  # Aggiungo il numero a una lista per poi manipolarlo
             index +=1
             continue
-    
-        b=np.array(ter_noti)  
+
+        b=np.array(ter_noti)
         m_t=np.reshape(b,[n_R,1]) # trasformo il vettore in una matrice
-    
+
         i=np.linalg.inv(m).dot(m_t)
-    
-        print('La soluzione del sistema lineare : \n\n',i)
-    
+
+        print('Solution of the linear system : \n\n',i)
+
         import cmath
         import math
-   
+
         indice=0
-        print('Le soluzioni in fasori del sistema lineare sono: \n')
+        print('Solution of the linear system in phasors: \n')
         for element in i:
             element=i[indice][0]
             i_f=cmath.polar(element)
@@ -74,51 +74,48 @@ while controllo =='si':
             indice+=1
             print(i_f[0],',',tetha)
 
-    controllo=input('Devi fare operazioni? (si/no) \n ')
-    
-    if controllo=='si':
-    
-        mem_m=input('Vuoi riutilizzare la stessa matrice? (si/no) \n')
-    
-        while mem_m=='si':
-            print('La matrice risulta essere: \n \n ',m)
-        
-            ter_noti=[] 
+    controllo=input('You have to do something? (y/n) \n ')
+
+    if controllo=='y':
+
+        mem_m=input('Do you want to reuse the same matrix? (y/n) \n')
+
+        while mem_m=='y':
+            print('Matrix : \n \n ',m)
+
+            ter_noti=[]
 
             index=0
 
             while index< n_R:
-                t=complex(input('Inserisci un numero (anche complesso): (R+Ij)\n\n'))
+                t=complex(input('Enter a value (even complessalex): (R+Ij)\n\n'))
                 ter_noti.append(t)  # Aggiungo il numero a una lista per poi manipolarlo
                 index +=1
                 continue
-    
-            b=np.array(ter_noti)  
+
+            b=np.array(ter_noti)
             m_t=np.reshape(b,[n_R,1]) # trasformo il vettore in una matrice
-    
+
             i=np.linalg.inv(m).dot(m_t)
-    
-            print('La soluzione del sistema lineare : \n\n',i)
-    
+
+            print('Solution of the linear system : \n\n',i)
+
             import cmath
             import math
-   
+
             indice=0
-            print('Le soluzioni in fasori del sistema lineare sono: \n')
+            print('Solution of the linear system in phasors: \n')
             for element in i:
                 element=i[indice][0]
                 i_f=cmath.polar(element)
                 tetha=math.degrees(i_f[1]) # definisco l'angolo in gradi
                 indice+=1
                 print(i_f[0],',',tetha)
-            
-            controllo=input('Devi fare operazioni? (si/no) \n ')
-            mem_m=input('Vuoi riutilizzare la stessa matrice? (si/no) \n')
-        
-    
-            
-    
+
+            controllo=input('You have to do something? (y/n) \n ')
+            mem_m=input('Do you want to reuse the same matrix? (y/n) \n')
+
+
+
+
 input('Press any button to quit.')
-
-
-
